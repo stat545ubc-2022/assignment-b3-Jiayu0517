885c9381dc05f13df4e4d79d8f89db16d12ff847
Hart <h1252048694@126.com>
2022-11-25 13:40:46 -0800

    app.R done

--- app.R
+++ app.R
@@ -5,6 +5,8 @@ library(dplyr)
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
 ui <- fluidPage(
+  ## Feature 1: Added a BC Liquor Store image
+  img(src="download.png", height="15%", width="15%"),
   titlePanel("BC Liquor Store prices"),
   sidebarLayout(
     sidebarPanel(
@@ -12,29 +14,74 @@ ui <- fluidPage(
       radioButtons("typeInput", "Product type",
                    choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
                    selected = "WINE"),
-      selectInput("countryInput", "Country",
-                  choices = c("CANADA", "FRANCE", "ITALY"))
+      ## Feature 2: sort data by alcohol content
+      checkboxInput("sortInput", "Sort results by alcohol content", TRUE),
+      uiOutput("countryOutput")
     ),
     mainPanel(
-      plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
+      ## Feature 3: User can download the data table to local machine
+      downloadLink('downloadData', 'Click to download the table'),
+
+      ## Feature 4: Modified tab panel to contain 2 tabs for plot and table
+      tabsetPanel(
+        tabPanel("Plot", plotOutput("coolplot")),
+        tabPanel("Table", tableOutput("results"))
+      )
     )
   )
 )
 
 server <- function(input, output) {
-  output$coolplot <- renderPlot({
-    filtered <-
+  output$countryOutput <- renderUI({
+    selectInput("countryInput", "Country",
+                sort(unique(bcl$Country)),
+                selected = "CANADA")
+  })
+
+  filtered <- reactive({
+    if (is.null(input$countryInput)) {
+      return(NULL)
+    }
+
+    random <-
       bcl %>%
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
              Type == input$typeInput,
              Country == input$countryInput
       )
-    ggplot(filtered, aes(Alcohol_Content)) +
+
+    # Feature 2: sort table by alcohol content
+    if (input$sortInput) {
+      random %>%
+        arrange(Alcohol_Content)
+    } else {
+      random
+    }
+
+  })
+
+  output$coolplot <- renderPlot({
+    if (is.null(filtered())) {
+      return()
+    }
+    ggplot(filtered(), aes(Alcohol_Content)) +
       geom_histogram()
   })
+
+  output$results <- renderTable({
+    filtered()
+  })
+
+  ## Feature 3: download table to local machine
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste('data-BCliquor.csv', sep='')
+    },
+    content = function(con) {
+      write.csv(filtered(), con)
+    }
+  )
 }
 
 shinyApp(ui = ui, server = server)
